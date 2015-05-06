d02d76e7b13700f4dc7169e1c9659c04452e6fea
Michael <email@tabiku.com>
2015-05-06 21:33:20 +1000

    upd

--- project/static/popup.js
+++ project/static/popup.js
@@ -1,4 +1,1 @@
-$(window).load(function(){var theWindow=$(window);function posPopup(){if($(window).width()>750){$('.popup').css('height','400px');}
-else{$('.popup-bar').css('left',0);$('.popup-bar').css('top','50px');}
-boxWidth=$('.popup').width();boxHeight=$('.popup').height();winWidth=$(window).width();winHeight=$(window).height();width=(winWidth-boxWidth)/ 2;height=(winHeight-boxHeight)/ 2;$('.popup').css({'left':width+'px','top':height+'px'});}
-theWindow.resize(posPopup).trigger("resize");});
+$(window).load(function(){var theWindow=$(window);function posPopup(){if($(window).width()>750){$('.popup').css('height','400px');}else{$('.popup-bar').css('left',0);$('.popup-bar').css('top','50px');}boxWidth=$('.popup').width();boxHeight=$('.popup').height();winWidth=$(window).width();winHeight=$(window).height();width=(winWidth-boxWidth)/ 2;height=(winHeight-boxHeight)/ 2;$('.popup').css({'left':width+'px','top':height+'px'});}theWindow.resize(posPopup).trigger("resize");});
